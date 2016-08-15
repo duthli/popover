94efddd73b3c7f95490ff3ff7bb165abdf4921ac
Linh Bội <hung@macbook-pro-cua-do.local>
2016-08-15 09:15:23 +0700

    Initial Commit

---
+++ popoverView/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  popoverView
+//
+//  Created by do duy hung on 15/08/2016.
+//  Copyright © 2016 do duy hung. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
